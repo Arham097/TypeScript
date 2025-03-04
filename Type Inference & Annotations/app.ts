a37545cf66fcc4e98e2072ea4511ef199e5f7c77
Arham097 <arhamhasan70@gmail.com>
2025-03-05 00:08:28 +0500

    type inference and annotations completed

---
+++ Type Inference & Annotations/app.ts
@@ -0,0 +1,9 @@
+// Type inference means that TypeScript can figure out the type of a variable for you.
+
+let number1 = 10; // TypeScript will automatically infer that a is of type number
+let msg1 = "Hello"; // TypeScript will automatically infer that b is of type string
+
+// Type Annotations are a way to tell TypeScript what type a value is.
+
+let number2: number = 10; // Here we are explicitly telling TypeScript that a is of type number
+let msg2: string = "Hello"; // Here we are explicitly telling TypeScript that b is of type string
